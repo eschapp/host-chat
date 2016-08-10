136ab6e0ef840d0004ab31b9d4622a70a5ebf804
Eric Schapp <eschapp@gmail.com>
2016-08-10 13:27:32 -0500

    still working

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,18 @@ var config = getConfig({
   clearBeforeBuild: '!(images|favicon.ico)'
 });
 
+html: function (context) {
+    console.log(context.defaultTemplate({
+      head: '<base href="/" />'
+    }));
+    return {
+      'index.html': context.defaultTemplate({
+        head: '<base href="/" />'
+      })
+    }
+  }
+});
+
 config.plugins.push(
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
